a8a8a4550ba24d027f5cf4d2675a05204d51e679
retr0 <sebasmorandez@hotmail.com>
2024-03-09 18:11:47 -0600

    Separate login and register forms in sections. Modify anchor style. Create onclick function to switch form.

---
+++ html/login/js/main.js
@@ -0,0 +1,9 @@
+/**
+ * Shows and hides the given section ids
+ * @param {string} sectionIdToShow
+ * @param {string} sectionIdToHide
+ */
+function switchLoginFormSection(strsectionIdToShow, sectionIdToHide) {
+    document.getElementById(sectionIdToShow).style.display = "block";
+    document.getElementById(sectionIdToHide).style.display = "none";
+}
